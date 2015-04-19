87d0396d9b74c50998691d46df3547279c0bbcc8
The Great J <jeffrey410469@gmail.com>
2015-04-19 19:22:00 +0800

    fixed no user_group_ship in group/show

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,12 +1,14 @@
 module GroupsHelper
   def user_group_ship_state(group, user_group_ship)
-    case user_group_ship.state
-    when "pending"
-      "已經發出邀請，正在等待發起人同意"
-    when "approved"
-      "已加入" unless group.organizer == current_user
-    when "rejected"
-      "發起人拒絕您的加入"
+    if user_group_ship.present?
+      case user_group_ship.state
+      when "pending"
+        "已經發出邀請，正在等待發起人同意"
+      when "approved"
+        "已加入" unless group.organizer == current_user
+      when "rejected"
+        "發起人拒絕您的加入"
+      end
     end
   end
 end
