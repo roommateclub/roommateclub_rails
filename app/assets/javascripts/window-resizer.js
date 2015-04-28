57da289ca47fa3a9f685584a934688b60774226b
rolajeffrey <Jeffrey@rolajeffreydeiMac.local>
2015-04-28 14:39:52 +0800

    add dynamic window size support

--- app/assets/javascripts/window-resizer.js
+++ app/assets/javascripts/window-resizer.js
@@ -1,3 +1,6 @@
 $(document).on("page:change", function(){
   $('.content-wrapper').css("min-height", $(document).height() - $('.navbar').outerHeight() - $('#footerwrap').outerHeight());
+  $(window).resize(function(){
+      $(".content-wrapper").css("min-height", $(window).height() - $('.navbar').outerHeight() - $('.footer').outerHeight());
+  })
 })
